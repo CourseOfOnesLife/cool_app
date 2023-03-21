71e773dd8a8bf7a97a913e5fe16c57d0ed251725
Ugur Akdogan <balicienugur@gmail.com>
2023-03-21 14:33:03 +0300

    modified:   src/components/profile/exams.js 	modified:   src/components/profile/languages.js 	modified:   src/components/profile/skills.js

--- src/components/profile/exams.js
+++ src/components/profile/exams.js
@@ -13,10 +13,10 @@ const ExamsComponent = ( ) => {
         <div className="w-full h-full  pb-14 overflow-hidden">
             <div className=" flex flex-row p-3  w-full h-full space-x-5">
                 {Exams.slice(0,5).map((exam) => (
-                    <div key={exam.id} className="bg-[#335708] rounded-2xl w-full h-full flex flex-col">
+                    <div key={exam.id} className={`bg-${exam.status === 'success' ? '[#335708]' : 'gray-200'} rounded-2xl w-full h-full flex flex-col`}>
                         <div className="text-[1.2rem] max-w-[1rem] font-semibold text-white pt-2 pb-4 pl-3">{exam.syntax}</div>
-                        <div className=" w-full h-0.5 bg-[#86EE03]" />
-                        <p className="w-full h-full text-[#86EE03] font-semibold text-[1.2rem] flex justify-end items-end pb-2  pr-2 "><span className="text-4xl pr-0.5">{exam.score}</span>/100</p>
+                        <div className={`w-full h-0.5 bg-${exam.status === 'success' ? '[#86EE03]' : '[#335708]'} `} />
+                        <p className={`w-full h-full  font-semibold text-${exam.status === 'success' ? '[#86EE03]' : '[#335708]'} text-[1.2rem] flex justify-end items-end pb-2  pr-2`}><span className="text-4xl pr-0.5">{exam.score}</span>/100</p>
                     </div>
 
                 ))}
--- src/components/profile/skills.js
+++ src/components/profile/skills.js
@@ -1,11 +1,31 @@
 const Skills = () => {
     return (
-        <div className="bg-white  h-full w-3/12 rounded-2xl  border-2">
+        <div className="bg-white  h-full w-3/12 rounded-2xl pb-14  border-2">
             <p className="text-xl font-semibold text-[#582270] mt-3 ml-4">Skiller</p>
             <div className="w-full h-0.5 bg-gray-200 mt-3"></div>
+            <div className="h-full w-full grid grid-cols-2 grid-rows-2  gap-2 p-4">
+                <div className="h-full w-full py-2 px-1 bg-[#582270] rounded-2xl flex content-center">
+                    <p className="font-semibold text-md px-3 text-white">Javascript</p>
+                </div>
+                <div className="h-full w-full py-2 px-1 bg-[#582270] rounded-2xl flex content-center">
+                    <p className="font-semibold text-md px-3 text-white">SQL</p>
+                </div>
+                <div className="h-full w-full py-2 px-1 bg-[#582270] rounded-2xl flex content-center">
+                    <p className="font-semibold text-md px-3 text-white">HTML-CSS</p>
+                </div>
+                <div className="h-full w-full py-2 px-1 bg-[#582270] rounded-2xl flex content-center">
+                    <p className="font-semibold text-md px-3 text-white">React</p>
+                </div>
+   
+        
+                
+                
+
+                
+            </div>
 
         </div>
     )
 }
 
-    export default Skills;
+export default Skills;
