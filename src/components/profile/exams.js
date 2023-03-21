21331aa10e0be8945f05022dfdeab46aa58da61b
Ugur Akdogan <balicienugur@gmail.com>
2023-03-21 14:56:54 +0300

    modified:   src/components/profile/exams.js

--- src/components/profile/exams.js
+++ src/components/profile/exams.js
@@ -14,7 +14,7 @@ const ExamsComponent = ( ) => {
             <div className=" flex flex-row p-3  w-full h-full space-x-5">
                 {Exams.slice(0,5).map((exam) => (
                     <div key={exam.id} className={`bg-${exam.status === 'success' ? '[#335708]' : 'gray-200'} rounded-2xl w-full h-full flex flex-col`}>
-                        <div className="text-[1.2rem] max-w-[1rem] font-semibold text-white pt-2 pb-4 pl-3">{exam.syntax}</div>
+                        <div className={`text-[1.2rem] max-w-[1rem] font-semibold text-${exam.status === 'success' ? 'white' : '[#335708]'} pt-2 pb-4 pl-3`}>{exam.syntax}</div>
                         <div className={`w-full h-0.5 bg-${exam.status === 'success' ? '[#86EE03]' : '[#335708]'} `} />
                         <p className={`w-full h-full  font-semibold text-${exam.status === 'success' ? '[#86EE03]' : '[#335708]'} text-[1.2rem] flex justify-end items-end pb-2  pr-2`}><span className="text-4xl pr-0.5">{exam.score}</span>/100</p>
                     </div>
