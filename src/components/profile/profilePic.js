80839849166901ade5ac8ffb87fe6f2148c10838
Ugur Akdogan <balicienugur@gmail.com>
2023-03-23 18:55:50 +0300

    modified:   src/components/profile/profilePic.js

--- src/components/profile/profilePic.js
+++ src/components/profile/profilePic.js
@@ -2,7 +2,7 @@ import ProfilePicture from "../../assets/profilePic.jpg";
 
 const ProfilePic = () => {
     return (
-        <div className="bg-white  h-full w-[25rem]  max-xl:w-3/12 max-xl:h-[17rem] rounded-2xl border-2 flex justify-center items-center overflow-hidden transition-all  filter grayscale-0 hover:grayscale-[20%] ">
+        <div className="bg-white  h-full w-[22rem]  max-xl:w-3/12 max-xl:h-[17rem] rounded-2xl border-2 flex justify-center items-center overflow-hidden transition-all  filter grayscale-0 hover:grayscale-[20%] ">
             <img className="object-cover aspect-square h-full  w-full" src={ProfilePicture} alt="Profile Pic" />
             <div className="z-50 absolute flex flex-col w-full h-full  justify-end items-end  pb-4 pr-4">
                 <p className="text-lg  text-white font-semibold transition-all ">Tayfun Sönmez, 24</p>
