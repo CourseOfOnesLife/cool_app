9d4a4af34b25d176ae56118d627febfce46f70f3
Ugur Akdogan <balicienugur@gmail.com>
2023-03-27 17:59:23 +0300

    modified:   src/components/profile/experiences.js

--- src/components/profile/experiences.js
+++ src/components/profile/experiences.js
@@ -7,10 +7,16 @@ const Experience = () => {
             <p className="text-xl font-semibold text-[#335708] mt-3 ml-4">Deneyimler</p>
             <div className="w-full h-0.5 bg-gray-200 mt-3"></div>
             <div className="w-full h-full p-4 pb-[4.5rem] flex flex-row space-x-5">
-               
-            
 
-                {ExperienceData.slice(0, 2).map((experience, index) => (
+                {window.innerWidth > 510 ? ExperienceData.slice(0, 4).map((experience, index) => (
+                    <div key={index} className="flex flex-col h-full w-full bg-[#335708] rounded-2xl">
+                        <p className="text-white  text-[1.2rem] max-xl:text-[1.2rem] max-xl:pb-8 max-xl:max-w-[120px] font-semibold pt-2 pl-3 max-w-[120px] pb-7">{experience.title}</p>
+                        <div className="w-full h-0.5 bg-[#86EE03]" />
+                        <div className="flex justify-end content-end text-[#86EE03] font-semibold text-xl  pt-2 pr-3">{experience.company}</div>
+                        <div className="flex justify-end content-end text-[#86EE03] font-semibold text-sm italic  pt-0 pr-3">{experience.startDate}-{experience.endDate}</div>
+                    </div>
+
+                )) : ExperienceData.slice(0, 2).map((experience, index) => (
                     <div key={index} className="flex flex-col h-full w-full bg-[#335708] rounded-2xl">
                         <p className="text-white  text-[1.2rem] max-xl:text-[1.2rem] max-xl:pb-8 max-xl:max-w-[120px] font-semibold pt-2 pl-3 max-w-[120px] pb-7">{experience.title}</p>
                         <div className="w-full h-0.5 bg-[#86EE03]" />
@@ -18,10 +24,6 @@ const Experience = () => {
                         <div className="flex justify-end content-end text-[#86EE03] font-semibold text-sm italic  pt-0 pr-3">{experience.startDate}-{experience.endDate}</div>
                     </div>
                 ))}
-
-
-
-
             </div>
 
         </div>
