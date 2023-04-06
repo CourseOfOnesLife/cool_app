be58387e7fe557df3b9cfe8d03f22da1697cab37
Ugur Akdogan <balicienugur@gmail.com>
2023-04-06 18:01:25 +0300

    modified:   package-lock.json 	modified:   package.json 	new file:   src/authSlice.js 	new file:   src/components/Login.js 	new file:   src/components/SignUp.js 	modified:   src/index.js

---
+++ src/components/SignUp.js
@@ -0,0 +1,9 @@
+const SignUp = () => {
+    return (
+        <div className="h-screen w-full bg-gray-400 ">
+            
+        </div>
+    );
+}
+
+export default SignUp;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import Exams from './components/Exams';
 import Navbar from './components/Navbar';
-
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -18,7 +19,8 @@ root.render(
     <BrowserRouter>
     <Navbar />
       <Routes>
-
+        <Route path='/login' element={ <Login/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
         <Route path="/" exact element={<Home />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/exams" element={<Exams />}></Route>
@@ -26,3 +28,4 @@ root.render(
     </BrowserRouter>
   </div>
 );
+
